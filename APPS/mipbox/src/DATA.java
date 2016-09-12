ac3a4f5cebec6e79b7b6845179e3efddfb9a3844
Ju Ouyang <juouyang@gmail.com>
2016-09-12 20:34:42 +0800

    [mipbox] parse pathology report, first commit

--- APPS/mipbox/src/DATA.java
+++ APPS/mipbox/src/DATA.java
@@ -1,55 +1,104 @@
-package mip.util;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
-import org.apache.commons.codec.binary.Base64;
+/**
+ *
+ * @author ju
+ */
+public class DATA {
 
-public class Base64Utils {
+    static String[] breastSearchStrings = {"breast", "Lymph node", "nipple", "HER-2", "HER2"};
+    static String[] malignantTypes = {"invasive ductal carcinoma", "ductal carcinoma in situ"/*, "invasive lobular carcinoma"*/};
+    static String[] benignSearchStrings = {"benign", "fibroadenoma", "fibroadipose", "no carcinoma", "no metastasis", "negative for carcinoma"};
 
-    private final static byte[] key = "MIP4J.TCU.EDU.TW".getBytes();
+    public static void main(String args[]) throws UnsupportedEncodingException, IOException {
+        final Base64.Decoder decoder = Base64.getDecoder();
+        try (BufferedReader in = new BufferedReader(new FileReader("/home/ju/TMUH_DATA"))) {
 
-    public static String encrypt(String strToEncrypt) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            final String encryptedString = Base64.encodeBase64String(cipher.doFinal(strToEncrypt.getBytes()));
-            return encryptedString;
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            StringBuilder sb = new StringBuilder();
+            while (true) {
+                String encodedText = in.readLine();
+                if (encodedText == null) {
+                    break;
+                }
+                //out.accept(encodedText);
+                String decodeText = new String(decoder.decode(encodedText), "UTF-8");
+                String[] lines = decodeText.split("\n");
+                boolean isDiagnosisStart = false;
+                boolean isDiagnosisEnd = true;
+                String diagnosis = "";
+                String pathologyID = "";
+                String biopsyDate = "";
+                //int i = 0;
+                for (String s : lines) {
+                    //out.accept(++i + "\t" + s);
+                    if (s.contains("病理切片報告病理號碼")) {
+                        String[] tokens = s.split("：");
+                        pathologyID = (tokens.length == 2) ? tokens[1] : "ER0000000";
+                    }
+
+                    if (s.contains("切片日期")) {
+                        String[] tokens = s.split("：");
+                        biopsyDate = (tokens.length == 2) ? tokens[1] : "1900-01-01";
+                    }
+
+                    if (s.contains("病理診斷")) {
+                        isDiagnosisStart = true;
+                        isDiagnosisEnd = false;
+                    }
+
+                    if (s.contains("組織報告")) {
+                        isDiagnosisStart = false;
+                        isDiagnosisEnd = true;
+                        sb.append(pathologyID).append(",");
+                        sb.append(biopsyDate).append(",");
+                        diagnosis = StringUtils.replace(diagnosis, "\"", "'");
+                        sb.append("\"").append(diagnosis.trim()).append("\"").append(",");
+                        sb.append(StringUtils.containsIgnoreCase(diagnosis, "left") ? "V" : "-").append(",");
+                        sb.append(StringUtils.containsIgnoreCase(diagnosis, "right") ? "V" : "-").append(",");
+                        for (String malignantType : malignantTypes) {
+                            sb.append(StringUtils.containsIgnoreCase(diagnosis, malignantType) ? "V" : "-").append(",");
+                        }
+                        boolean isBenign = false;
+                        for (String searchString : benignSearchStrings) {
+                            if (StringUtils.containsIgnoreCase(diagnosis, searchString)) {
+                                sb.append("V");
+                                isBenign = true;
+                                break;
+                            }
+                        }
+                        sb.append(isBenign ? "" : "-").append(",");
+                        boolean isBreast = false;
+                        for (String searchString : breastSearchStrings) {
+                            if (StringUtils.containsIgnoreCase(diagnosis, searchString)) {
+                                sb.append("V");
+                                isBreast = true;
+                                break;
+                            }
+                        }
+                        sb.append(isBreast ? "" : "-").append(",");
+                        sb.append("\n");
+                    }
+
+                    if (isDiagnosisStart && !isDiagnosisEnd) {
+                        diagnosis += (s.trim().length() != 0) ? (s + "\n") : "";
+                    }
+                }
+
+                FileUtils.writeStringToFile(new File("/home/ju/test.csv"), sb.toString());
+            }
         }
-        return null;
-
-    }
-
-    public static String decrypt(String strToDecrypt) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-            final SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            final String decryptedString = new String(cipher.doFinal(Base64.decodeBase64(strToDecrypt)));
-            return decryptedString;
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
-        }
-        return null;
-    }
-
-    public static void main(String args[]) {
-
-        String strToEncrypt = "foo";
-        final String encryptedStr = Base64Utils.encrypt(strToEncrypt.trim());
-        System.out.println("String to Encrypt : " + strToEncrypt);
-        System.out.println("Encrypted : " + encryptedStr);
-
-        final String strToDecrypt = encryptedStr;
-        final String decryptedStr = Base64Utils.decrypt(strToDecrypt.trim());
-        System.out.println("String To Decrypt : " + strToDecrypt);
-        System.out.println("Decrypted : " + decryptedStr);
-
     }
 }
